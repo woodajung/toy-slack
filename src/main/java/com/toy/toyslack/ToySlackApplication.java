c7435ba0617cf498923da3bb1312b7343578eb54
woodajung <da2146@naver.com>
2023-09-07 16:30:55 +0900

    Initial commit

---
+++ src/main/java/com/toy/toyslack/ToySlackApplication.java
@@ -0,0 +1,13 @@
+package com.toy.toyslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySlackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/toyslack/ToySlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.toyslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
